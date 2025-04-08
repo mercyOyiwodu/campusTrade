b4fd740124d521a3b7043ded18eaecdcb6aa2aad
muabiodun99 <Muabiodun101@gmail.com>
2025-04-08 06:40:27 +0100

    resolving conflict

--- database/sequelize.js
+++ database/sequelize.js
@@ -1,8 +1,4 @@
-// const {Sequelize} = require('sequelize')
-// require('dotenv').config()
-// const host = process.env.DATABASE_HOST
-// const dialect = process.env.DATABASE_DIALECT
-// const sequelize = new Sequelize('sql12771825', 'sql12771825', '9j2xzi7K8I', {
+
     host: host,
     dialect: dialect
   });
