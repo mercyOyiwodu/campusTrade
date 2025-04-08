f5fce2cbd0f13f44fa0202a944d07869c4b80ea6
muabiodun99 <Muabiodun101@gmail.com>
2025-04-08 07:27:24 +0100

    made some change

--- database/sequelize.js
+++ database/sequelize.js
@@ -1,5 +1,9 @@
-
-    host: host,
+ const {Sequelize} = require('sequelize')
+ require('dotenv').config()
+const host = process.env.DATABASE_HOST
+ const dialect = process.env.DATABASE_DIALECT
+ const sequelize = new Sequelize('sql12771825', 'sql12771825', '9j2xzi7K8I', {
+   host: host,
     dialect: dialect
   });
 
