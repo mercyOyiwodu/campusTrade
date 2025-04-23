c36cacad9a33ca53cea3f12fe4b7a2c3e3500b49
muabiodun99 <Muabiodun101@gmail.com>
2025-04-23 15:17:19 +0100

    im happy

--- migrations/20250421152930-add-productId-to-transactions.js
+++ migrations/20250421152930-add-productId-to-transactions.js
@@ -2,13 +2,13 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Transactions', 'productId', {
+    await queryInterface.addColumn('transactions', 'productId', {
       type: Sequelize.UUID,
       allowNull: false,
       after: 'id'
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Transactions', 'productId');
+    await queryInterface.removeColumn('transactions', 'productId');
   }
 };
