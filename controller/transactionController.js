16a93d9aa170171c4060f136e8e7f520cd5a6a7b
muabiodun99 <Muabiodun101@gmail.com>
2025-04-24 10:46:02 +0100

    maybe

--- controller/transactionController.js
+++ controller/transactionController.js
@@ -36,7 +36,7 @@ exports.initializePayment = async (req, res) => {
             },
             currency: "NGN",
             reference:reference,
-            redirect_url: `https://campus-trade-h7bq.vercel.app/dashboard/paymentstatus/${reference}`,
+            redirect_url: `https://campus-trade-h7bq.vercel.app/dashboard/paymentstatus`,
             // redirect_url: `http://localhost:5173/dashboard/paymentstatus/${reference}`,
 
         };
